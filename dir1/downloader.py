3caac0dc595507af6185dd3184b8abb7190208da
Gustau Pérez <gustau.perez@gmail.com>
2018-11-02 18:32:31 +0100

    First version of the downloader script

---
+++ dir1/downloader.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import os.path
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+import threading
+import boto3
+
+class ownParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n\n' % message)
+        self.print_help()
+        sys.exit(1)
+
+def downloaderS3(resource, file_name, results):
+    path        = resource[0]
+    urlparsed   = resource[1]
+    print("About to download: " + file_name + " with: " + urlparsed.scheme)
+    try:
+        s3 = boto3.resource('s3')
+        s3.Bucket(urlparsed.netloc).download_file(urlparsed.path[1:], file_name)
+        results.update({file_name:"Ok"})
+    except Exception:
+        results.update({file_name:"Ko"})
+
+# First argument is a pair of [url, urlparsed], the second is the final filename we want and
+# the third is a hash we're gonna store the result of the download
+def downloaderHttp(resource, file_name, results):
+    path        = resource[0]
+    urlparsed   = resource[1]
+    print("About to download: " + file_name + " with: " + urlparsed.scheme)
+    try:
+        urlretrieve(path, file_name)
+        results.update({file_name:"Ok"})
+    except Exception:
+        results.update({file_name:"Ko"})
+
+schemes = {"http": downloaderHttp,
+           "https": downloaderHttp,
+           "s3": downloaderS3
+}
+
+# The main rutine will put into a hash table (dictionary) the names of the files to download as the key (to prevent the download
+# of the same file more than twice, the last one will take precedence. As the value we'ill put a tuple of the passed url and
+# the correspoding parsed url object (using urlparse).
+#
+# Also, to make the script easy to maintain, we have another hash table called schemes that contains all the protocols supported
+# and the correspoing routine that will handle the download. That way we can spawn a thread to handle the download and the routine
+# that the thread will run can be easily found in the schemes hash table.
+#
+if __name__ == '__main__':
+    files_to_download = {}
+    results           = {}
+    threads           = []
+    exit_code         = 0
+
+    parser = ownParser(description='Downloads a bunch of urls. The schemes supported are: '+' '.join(map(str, schemes.keys())))
+    parser.add_argument('-u', '--url', nargs='+', default=[], required=True)
+
+    args=parser.parse_args()
+
+    if args.url:
+        for value in args.__dict__["url"]:
+            try:
+                o = urlparse(value)
+                if schemes.get(o.scheme) and o.path != "" and o.path != "/":
+                    files_to_download.update({os.path.split(o.path)[1]:[value, o]})
+            except:
+                print("Wrong url: " + value, sys.exc_info()[0])
+                sys.exit(1)
+
+        for key in files_to_download:
+            t = threading.Thread(target=schemes.get(files_to_download.get(key)[1].scheme), args=(files_to_download.get(key), key, results))
+            threads.append(t)
+            t.start()
+
+        for x in threads:
+            x.join()
+
+        for key in results:
+            print("### File: " + key + " from: "+files_to_download.get(key)[0] + " downloaded with status: " + results.get(key))
+            if results.get(key) == "Ko":
+                exit_code = 1
+
+        sys.exit(exit_code)
